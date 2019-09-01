866d921ced321cc80de595110066331d459e95eb
Divnsh <divyansh.madhwal@gmail.com>
2019-09-01 21:42:30 +0530

    commit_2

--- feature eng.py
+++ feature eng.py
@@ -8,6 +8,7 @@ Created on Sat Aug 24 11:32:47 2019
 title: AV Hackathon WNS
 """
 
+
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split,cross_validate
@@ -19,8 +20,17 @@ from sklearn.metrics import f1_score,make_scorer
 from sklearn.decomposition import PCA
 from yellowbrick.classifier import ROCAUC ,ClassificationReport,ConfusionMatrix
 import os
+from datetime import timedelta,datetime as dt
+import multiprocessing
+from itertools import repeat
+from functools import reduce
+import gc
+import time
+from pathos.multiprocessing import ThreadingPool
+import traceback
 
-os.chdir('/home/vivek/Documents/kaggle_practice/AnalyticsVidya_WNS_clicks/')
+
+os.chdir('/home/divyansh/DataScience/Modelling/AV_WNS_Hackathon')
 color = sns.color_palette()
 score_fn=make_scorer(f1_score,average='weighted')
 
@@ -67,14 +77,21 @@ def predict_test_values(test_df,model,transformer):
     df['label']=predictions
     return  df          
 
-if __name__=='__main__':
+def load_data():
     train=pd.read_csv(r'./Data/train.csv')
+    train['impression_time']=pd.to_datetime(train['impression_time'])
     item=pd.read_csv(r'./Data/item_data.csv')
     item.head()
     log=pd.read_csv(r'./Data/view_log.csv')
+    log['server_time']=pd.to_datetime(log['server_time'])
     log.head()
     train.head()
     log = log.merge(item,on='item_id',how='inner')
+    return train,log
+    
+if __name__=='__main__':
+    train,log=load_data()
+    '''
     log,X_train=baseline_model(train)    
     test_df=pd.read_csv(r'./Data/test.csv')
     with open(r'./Models/baseline_logistic.pkl','rb') as f:
@@ -87,37 +104,94 @@ if __name__=='__main__':
     visualizer.score(X_train,train['label'])
     visualizer.poof()
     feature_series=pd.Series(index=X_train.columns,data=X_train.values)
+    '''  
+
+# Users split function
+#train=train[:10000]   
+users=train.user_id.unique()
+
+def users_split(k):
+    u=[]
+    for i in range(1,k+1):
+        u.append(users[(i-1)*int(len(users)/k):i*int(len(users)/k)])
+    if len(users)%k!=0:
+        u.append(users[i*int(len(users)/k):])
+    return u
+
+userslist = users_split(5) 
+
+# Checking for ads shown within 7 days
+
+def check_dates(row,user_id):
+    logdf = log.loc[log['user_id']==user_id].sort_values('server_time')
+    if len(logdf)==0:
+        return [[] for i in range(len(log.columns))]
+    is_ad = logdf.apply(lambda x:(row['impression_time']>=x['server_time']) & (row['impression_time']<=x['server_time']+timedelta(days=7)),axis=1)
+    #print(is_ad)
+    colnames = list(log.columns)
+    colnames.remove('server_time')
+    finlist = [list(is_ad.astype('int'))]
+    for col in colnames:
+        finlist.append(list(logdf[is_ad][col]))
+    #print(finlist)    
+    return finlist   
+
+# Tracking ads shown within 7 days
+print_lock= multiprocessing.Lock()    
+def track(u):
+    try:           
+        t=train.loc[train.user_id==u].copy()
+        t['is_tracked']= t.apply(lambda x:check_dates(x,u),axis=1)
+        return t
+    except:
+         with print_lock:
+            print("%s: An exception occurred" % u)
+            print(traceback.format_exc())
     
-from datetime import timedelta,datetime as dt
+def add_tracking(userslist):
+    print(userslist)
+    fdf = []
+        
+    with multiprocessing.Pool(processes=8) as pool:
+        fdf = pool.map(track, userslist)
+        pool.close()
+        pool.join()
+    return reduce(lambda x,y: pd.concat([x,y],axis=0),fdf)    
+
+start = time.time()
+
+result=ThreadingPool().map(add_tracking, list(userslist))
+
+'''
+with multiprocessing.Pool(processes=8) as pool:
+    result = pool.map(add_tracking, list(userslist))
+'''
+        
+'''
+fdf=[]
+for u in userslist:
+    res=add_tracking(u)
+    fdf.append(res)
+'''  
+end = time.time()       
+minutes=int((end-start)/60)
+seconds=np.round((end-start)%60,2)
+print("time taken: {} minutes and {} seconds".format(minutes,seconds))
+
+
+fdf = reduce(lambda x,y: pd.concat([x,y],axis=0),result)
+
+fdf=fdf.reset_index(drop=True)
+
+cols=list(log.columns)
+cols[0] = 'n_trackings'
+
+for i,col in enumerate(cols):
+    fdf[col]=fdf.is_tracked.apply(lambda x:x[i])
     
-v63410=train[train['user_id']==63410].sort_values('impression_time')
-l63410=log[log['user_id']==63410].sort_values('server_time')   
-
-
-def check_dates(row):
-    j=0
-    for i in pd.to_datetime(sorted(log[log['user_id']==63410]['server_time'])):
-        j=j+(pd.to_datetime(row['impression_time'])>i and pd.to_datetime(row['impression_time'])<i+timedelta(days=7)) 
-    return j     
-
-res=train[train['user_id']==63410].apply(lambda x:check_dates(x),axis=1)
-
-sorted(log[log['user_id']==63410]['server_time'])
-
-sorted(log[log['user_id']==63410]['server_time']).apply(lambda x:>=x, <x+timedelta(days=7))
-
-
-
-
-
-
-
-
-
-
-
-
-
+fdf.drop(['is_tracked'],axis=1).to_csv('/home/divyansh/DataScience/Modelling/AV_WNS_Hackathon/Data/train_new.csv',index=False,header=True)    
+    
+    
 
 
 
